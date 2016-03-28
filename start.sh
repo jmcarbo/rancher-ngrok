e0f077faa3403d22635a18c396f16e6f2f3ccfb6
Joan Marc Carbo <jmcarbo@gmail.com>
2016-03-28 10:58:59 +0200

    ...

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 export GODEBUG=netdns=cgo
 gosuv add --name "ngrok" -- /bin/ngrok start --all --config=/etc/ngrok.yml
 confd -onetime -backend rancher
+gosuv stop ngrok && gosuv start ngrok
 confd -backend rancher -interval ${CONFD_INTERVAL:-15}
