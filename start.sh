56f4374adabc59d429c8b6d83a8a2faea225a4c6
Joan Marc Carbo Arnau <joanmarc@gmail.com>
2016-03-31 15:22:50 +0200

    ..

--- start.sh
+++ start.sh
@@ -4,10 +4,9 @@ echo "" >/etc/consul.yml
 gosuv add --name "ngrok" -- /bin/ngrok start --all --config=/etc/ngrok.yml --config=/etc/consul.yml
 if [ ! -z ${CONSUL_URL} ]; 
 then
-  gosuv add --name "consul-template" -- /bin/consul-template --consul=${CONSUL_URL} --token=${CONSUL_TOKEN} -template "/etc/confd/templates/consul.yml.tmpl:/etc/consul.yml" 
+  gosuv add --name "consul-template" -- /bin/consul-template --consul=${CONSUL_URL} --token=${CONSUL_TOKEN} -template "/etc/confd/templates/consul.yml.tmpl:/etc/consul.yml:gosuv stop ngrok && gosuv start ngrok" 
 fi
 
 confd -onetime -backend rancher
-
 gosuv stop ngrok && gosuv start ngrok
 confd -backend rancher -interval ${CONFD_INTERVAL:-15}
