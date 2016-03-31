8c76d81f6abe3e60bd141bd78d09d8b9ff3ba446
Joan Marc Carbo Arnau <joanmarc@gmail.com>
2016-03-31 15:09:11 +0200

    ...

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 export GODEBUG=netdns=cgo
 echo "" >/etc/consul.yml
 gosuv add --name "ngrok" -- /bin/ngrok start --all --config=/etc/ngrok.yml --config=/etc/consul.yml
-if [ -n ${CONSUL_URL+1} ]; 
+if [ ! -z ${CONSUL_URL} ]; 
 then
-  gosuv add --name "consul-template" -- /bin/consul-templates --consul="$CONSUL_URL" --token="$CONSUL_TOKEN" -template "/etc/confd/templates/consul.yml.tmpl:/etc/consul.yml" 
+  gosuv add --name "consul-template" -- /bin/consul-template --consul=${CONSUL_URL} --token=${CONSUL_TOKEN} -template "/etc/confd/templates/consul.yml.tmpl:/etc/consul.yml" 
 fi
 
 confd -onetime -backend rancher
